e9172741a1d55498d343b14d2c1c97acf8961862
iwebnal <iwebnal@gmail.com>
2024-08-02 00:38:07 +0300

    Refactoring

--- app/src/main/java/com/ahatuhov/lemonade/MainActivity.kt
+++ app/src/main/java/com/ahatuhov/lemonade/MainActivity.kt
@@ -14,11 +14,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -26,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -82,6 +88,7 @@ fun TopBlock(modifier: Modifier = Modifier) {
 @Composable
 fun ContentBlock(modifier: Modifier = Modifier) {
     var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
 
     Box(
         modifier = modifier,
@@ -89,90 +96,83 @@ fun ContentBlock(modifier: Modifier = Modifier) {
     ) {
         when (currentStep) {
             1 -> {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_tree),
-                        contentDescription = "Clickable Image",
-                        modifier = Modifier
-                            .background(Color.Green, shape = RoundedCornerShape(20.dp))
-                            .padding(30.dp)
-                            .clickable { currentStep = 2},
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.str_lemon_tree),
-                        color = Color.Black,
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                }
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.str_lemon_tree,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.title_lemon_tree,
+                    onImageClick = {
+                        currentStep = 2
+                        squeezeCount = (2..4).random()
+                    })
             }
+
             2 -> {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_squeeze),
-                        contentDescription = "Clickable Image",
-                        modifier = Modifier
-                            .background(Color.Green, shape = RoundedCornerShape(20.dp))
-                            .padding(30.dp)
-                            .clickable { currentStep = 3},
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.str_keep_tappin),
-                        color = Color.Black,
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                }
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.str_keep_tappin,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionResourceId = R.string.title_lemon,
+                    onImageClick = {
+                        squeezeCount--
+                        if (squeezeCount == 0) {
+                            currentStep = 3
+                        }
+                    })
             }
+
             3 -> {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_drink),
-                        contentDescription = "Clickable Image",
-                        modifier = Modifier
-                            .background(Color.Green, shape = RoundedCornerShape(20.dp))
-                            .padding(30.dp)
-                            .clickable { currentStep = 4},
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.str_tap_the_lemonade),
-                        color = Color.Black,
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                }
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.str_tap_the_lemonade,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    contentDescriptionResourceId = R.string.title_glass_of_lemonade,
+                    onImageClick = {
+                        currentStep = 4
+                    })
             }
+
             4 -> {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.lemon_restart),
-                        contentDescription = "Clickable Image",
-                        modifier = Modifier
-                            .background(Color.Green, shape = RoundedCornerShape(20.dp))
-                            .padding(30.dp)
-                            .clickable { currentStep = 1},
-                        contentScale = ContentScale.Crop
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.str_empty_glass),
-                        color = Color.Black,
-                        style = TextStyle(fontSize = 16.sp)
-                    )
-                }
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.str_empty_glass,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    contentDescriptionResourceId = R.string.title_empty_glass,
+                    onImageClick = {
+                        currentStep = 1
+                    })
             }
         }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = onImageClick,
+            shape = RoundedCornerShape(dimensionResource(R.dimen.button_corner_radius)),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer)
+        ) {
+            Image(
+                painter = painterResource(drawableResourceId),
+                contentDescription = stringResource(contentDescriptionResourceId),
+                modifier = Modifier
+                    .padding(30.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(textLabelResourceId),
+            color = Color.Black,
+            style = TextStyle(fontSize = 16.sp)
+        )
     }
 }
 
